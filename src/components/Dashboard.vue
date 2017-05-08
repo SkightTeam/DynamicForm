5259f59373cf8f0dc24114bc84b5b83cac4d723c
王昊 <wanghao@skight.com>
2017-05-07 22:05:06 -0700

    Add Dashboard and skForm

---
+++ src/components/Dashboard.vue
@@ -0,0 +1,45 @@
+<template >
+
+ <div>
+   <h1>Dynamic Form Editor</h1>
+   <sk-form :meta="schema"></sk-form>
+ </div>
+</template>
+
+<script>
+  import SkForm from './skForm'
+  export default {
+    name: 'Dashboard',
+    components: {
+      SkForm
+    },
+    data () {
+      return {
+        schema: [
+          {
+            code: 'code',
+            name: '编码',
+            type: 'string'
+          },
+          {
+            code: 'name',
+            name: '名称',
+            type: 'string'
+          },
+          {
+            code: 'type',
+            name: '类型',
+            type: 'string'
+          }
+        ]
+      }
+    }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+  h1, h2 {
+    font-weight: normal;
+  }
+</style>
---
+++ src/components/skForm.vue
@@ -0,0 +1,21 @@
+<template >
+ <div>
+   <div v-for="item in meta">{{item.name}}</div>
+ </div>
+</template>
+
+<script>
+export default {
+  name: 'SkForm',
+  props: [
+    'meta'
+  ]
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+  h1, h2 {
+    font-weight: normal;
+  }
+</style>
